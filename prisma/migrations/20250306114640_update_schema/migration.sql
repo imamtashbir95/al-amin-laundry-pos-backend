ecd3b07f97c36cecf820b39e800f755ec88cfe44
imamtashbir95 <imamtashbir95@gmail.com>
2025-03-08 04:04:59 +0700

    Prisma migrations regeneration

--- prisma/migrations/20250306114640_update_schema/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `is_deleted` on the `BillDetail` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "BillDetail" DROP COLUMN "is_deleted";
